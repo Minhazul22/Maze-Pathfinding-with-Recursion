1e281534b1f1bcf62ad053a7c4172cac5cff07f6
MINHAZUL ISLAM <mdi42473@gmail.com>
2026-01-07 17:38:30 +0100

    Add maze pathfinding solution using recursion and backtracking

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class MazeSolver{
+    private static final char WALL = '#';
+    private static final char FREE = ' ';
+    private static final char PATH = '.';
+    private static final char START = 'S';
+    private static final char END = 'E';
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+    private char[][] maze = {
+            {'#', '#', '#', '#', '#', '#', '#'},
+            {'#', 'S', ' ', '#', ' ', ' ', '#'},
+            {'#', ' ', '#', '#', ' ', '#', '#'},
+            {'#', ' ', ' ', ' ', ' ', '#', '#'},
+            {'#', '#', '#', ' ', '#', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', 'E', '#'},
+            {'#', '#', '#', '#', '#', '#', '#'}
+
+    };
+
+
+    public static void main(String[] args){
+        MazeSolver Solver = new MazeSolver();
+
+
     }
+
+
+
 }
